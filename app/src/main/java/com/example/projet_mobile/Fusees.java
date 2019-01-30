66b4a005b32fa91e7bb4b78a56b73c24f7120db3
CharlesPilon <pilon@et.esiea.fr>
2019-01-30 09:31:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projet_mobile/Fusees.java
@@ -0,0 +1,15 @@
+package com.example.projet_mobile;
+
+public class Fusees {
+
+    public Fusees(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
+

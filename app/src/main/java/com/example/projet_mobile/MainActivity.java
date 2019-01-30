abee1da23efeacbd9c37251795f480388eb87b53
Charles <charlespilon@me.com>
2019-01-30 12:01:51 +0100

    Modifications de la seconde activité

--- app/src/main/java/com/example/projet_mobile/MainActivity.java
+++ app/src/main/java/com/example/projet_mobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.projet_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startSecondActivity(){
+        Intent secondActivity = new Intent(this, SecondActivity.class);
+        startActivity(secondActivity);
     }
 }
